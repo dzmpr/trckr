9add322a36b8fdfe29a991aad25af65ae1307246
dzmitry <mr.priskoka@yandex.com>
2022-09-18 11:08:09 +0300

    Update primitives converter

--- core/src/main/kotlin/ru/cooked/trckr/core/converter/PrimitivesConverter.kt
+++ core/src/main/kotlin/ru/cooked/trckr/core/converter/PrimitivesConverter.kt
@@ -2,14 +2,14 @@ package ru.cooked.trckr.core.converter
 
 internal class PrimitivesConverter : TypeConverter {
 
-    override fun convert(value: Any?): String? = when (value) {
+    override fun convert(value: Any?): Any? = when (value) {
         null -> "null"
-        is String -> value
+        is String,
         is Long,
         is Int,
         is Float,
         is Double,
-        is Boolean -> value.toString()
+        is Boolean -> value
         else -> null
     }
 }
