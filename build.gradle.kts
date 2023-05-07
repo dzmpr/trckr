90650d1be7e1d49337f19c36103b10289b163535
dzmitry <mr.priskoka@yandex.com>
2023-05-07 15:10:56 +0300

    TRCKR-T-10 add public api validation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,15 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.binaryvalidator)
+}
+
+apiValidation {
+    with(ignoredProjects) {
+        add("trckr-demo")
+        add("trckr-processor")
+    }
+    nonPublicMarkers.add("ru.cookedapp.trckr.core.annotations.internal.TrckrInternal")
 }
 
 allprojects {
